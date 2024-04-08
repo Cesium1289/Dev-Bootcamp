c14190d2dc0e6b41495e7ed30fd8f57308f68d73
Carson Hansen <92325833+Cesium1289@users.noreply.github.com>
2024-04-08 11:18:00 -0700

    Connect app to DB
    -App can connect to DB though mongoose

--- server.js
+++ server.js
@@ -1,20 +1,28 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const morgan = require("morgan");
+const connectDB = require("./config/db");
+//load env vars
+dotenv.config({ path: "./config/config.env" });
 
 //Route bootcamps
 const bootcamps = require("./routes/bootcamps");
 
-//load env vars
-dotenv.config({ path: "./config/config.env" });
-
+//connec to database
+connectDB();
 const app = express();
+
+//Dev logging middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("default"));
+}
+
+//mount routers
 app.use("/api/v1/bootcamps", bootcamps);
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
-  console.log(
-    PORT,
-    `server runnning in ${process.env.NODE_ENV} mode on port ${PORT}`
-  )
+  console.log(`server runnning in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
