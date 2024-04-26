b433e297cfdabc6df295d601e91a3e4b8206a09a
Carson Hansen <92325833+Cesium1289@users.noreply.github.com>
2024-04-26 11:43:47 -0700

    Add geolocation field to db
    -create geolocation based on address of bootcamp

---
+++ utils/geocoder.js
@@ -0,0 +1,13 @@
+const { model } = require("mongoose");
+const nodeGeoCoder = require("node-geocoder");
+
+const options = {
+  provider: process.env.GEOCODER_PROVIDER,
+  httpsAdapter: "https",
+  apiKey: process.env.GEOCODER_API_KEY,
+  formatter: null,
+};
+
+const geoCoder = nodeGeoCoder(options);
+
+module.exports = geoCoder;
